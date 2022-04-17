d8269fd3c81fa2d29475ca74d026fc78764171cc
“tomh543” <tomh543@hotmail.co.uk>
2022-04-17 13:51:26 +0100

    Update ec2tos3.py

--- code/ec2tos3.py
+++ code/ec2tos3.py
@@ -1,3 +1,4 @@
+#Student Name Tom Hope, ID : S1311448
 #import glob for searching file types
 #import os for filepaths
 #import time to set delays
@@ -8,12 +9,12 @@ import os
 import time
 
 BUCKET_NAME = 'coursework-bucket-s1311448'
-FOLDER_NAME = 'CPDCourseworkImagesS1311448/images/'
+FOLDER_NAME = 'CPDImages/images/'
 
 s3 = boto3.resource('s3')
-jpg_files = glob.glob("CPDCourseworkImagesS1311448/images//images/*.jpg")
-png_files = glob.glob("CPDCourseworkImagesS1311448/images//images/*.png")
-#for future updates addition of new file formats would be done by adding e.g. gif_files = glob.glob("CPDCourseworkImagesS1311448/images//images/*.")
+jpg_files = glob.glob("CPDImages/images/*.jpg")
+png_files = glob.glob("CPDImages/images/*.png")
+#for future updates addition of new file formats would be done by adding e.g. gif_files = glob.glob(""CPDImages/images/*.")
 
 #checks for jpg files and uploads with a timer delay of 30 seconds between each upload
 for filename in jpg_files:
@@ -28,10 +29,11 @@ for filename in png_files:
     print("Putting %s as %s" % (filename,key))
     s3.meta.client.upload_file(filename, BUCKET_NAME, key)
     time.sleep(30)
+#For future updates adding a new for function(s) for filetype_files would be added 
 #Check to see if any files are not jpg or png. If file exists notify user.  
 if filename != jpg_files or filename != png_files:
     print ("File not in correct format png & jpg only")
     
-#For future updates adding a new for function(s) for filetype_files would be added     
+    
 #Print Upload complete and inform users only two file types supported.
 print("Upload Complete, Please note only .jpg and .png files are supported any othell not be uploaded")
